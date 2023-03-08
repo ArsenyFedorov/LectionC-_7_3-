2309a9e1f9e2cc334cea15a460530b78e1a222c7
Арсений <ars071026@example.com>
2023-03-08 12:19:23 +0500

    Задача 3 решена

---
+++ Leson_1/HW/Task_1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write.Line("Ввидите первое число"!);
+int z = int.Parse(Console.ReadLine());
+Console.Write.Line("Ввидите второе  число"!);
+int x = int.Parse(Console.ReadLine());
+Console.Write.Line("Ввидите третье число"!);
+int c = int.Parse(Console.ReadLine());
+if (z > x ){
+     if (z > c)
+    Console.WriteLine(z);
+    }
+    else if (c > z)
+    Console.WriteLine(c);
+
+else if (z < x){
+    if (x > c)
+    Console.WriteLine(x);
+    }
+    else if (c > x)
+    Console.Write.Line(c);
---
+++ Leson_1/HW/Task_1_2/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Ввидите число:");
+int a = int.Parse(Console.ReadLine()!);
+if (a %2==0 ) 
+Console.WriteLine("Чётное ");
+else 
+Console.WriteLine("Не чётное");
